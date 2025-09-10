6ca5872a1a27f8c702d616a442c44add48d02d0f
JNACHO2001 <113179500+JNACHO2001@users.noreply.github.com>
2025-09-10 15:19:54 -0500

    feat:adaptacion de funciones

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,28 +1,38 @@
 import type { Request, Response } from "express";
-// llamamos al modelo para hacer referencia a la tabla 
+// llamamos al modelo para hacer referencia a la tabla
 import User from "../models/user";
 import { UserCreationAttributes } from "../interfaces/user.interface";
 
-export const getUsers = async (_req: Request, res: Response) => {
+export async function getUsers(_req: Request, res: Response) {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ message: "Usuarios encontrados", data: users });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al obtener los usuarios" });
+  }
+}
 
-  // este es el metodo de sequieslize para enlistar todos los usuarios de la tabla
-  // dentro del modelo de findall podemos agregar los atributos que deseamos llamar{atrtibutes:[nombre]}
-  const users = await User.findAll();
-  res.status(200).json({ message: "usuarios encontrados", data: users });
-};
-// este metodo es para buscar un usuario de manera individual 
-export const getUser = async (req: Request, res: Response) => {
-  const user = await User.findByPk(req.params.id);
-  if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
-  res.json(user);
-};
+// este metodo es para buscar un usuario de manera individual
+
+export async function getUser(req: Request, res: Response) {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) return res.status(404).json({ message: "no hay usuarios" });
+
+    return res.status(200).json({ message: "usuario encontrado", data: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error en el servidor" });
+  }
+}
 
 export const createUser = async (req: Request, res: Response) => {
   try {
-    // este es el metodo para insetar usuarios es las tablas 
-    const data:UserCreationAttributes=req.body
+    // este es el metodo para insetar usuarios es las tablas
+    const data: UserCreationAttributes = req.body;
     const user = await User.create(data);
-    res.status(201).json({ message: "fue creado", data:user.name });
+    res.status(201).json({ message: "fue creado", data: user.name });
   } catch (error) {
     res.status(500).json({ error, message: "no fue posible la creacion" });
   }
@@ -34,15 +44,15 @@ export const updateUser = async (req: Request, res: Response) => {
   if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
   // este metodo es para actulizar el usuario encontrado
   await user.update(req.body);
-  res.status(201).json({message:"fue actualizado ",data:user});
+  res.status(201).json({ message: "fue actualizado ", data: user });
 };
-
 
 export const deleteUser = async (req: Request, res: Response) => {
   const user = await User.findByPk(req.params.id);
   if (!user) return res.status(404).json({ message: "Usuario no encontrado" });
   // este metodo es para eliminar el usuario encontrado
   await user.destroy();
-  res.status(200).json({message:"el usuario ha sido eliminado",name:user.name});
+  res
+    .status(200)
+    .json({ message: "el usuario ha sido eliminado", name: user.name });
 };
-
